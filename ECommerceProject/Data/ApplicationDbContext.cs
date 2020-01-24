25abb073f7872502200b2346208e3e1127f17e12
Unuero Potential <unueroavwerosuo@gmail.com>
2020-01-24 15:54:33 +0100

    Added Models

--- ECommerceProject/Data/ApplicationDbContext.cs
+++ ECommerceProject/Data/ApplicationDbContext.cs
@@ -8,9 +8,20 @@ namespace ECommerceProject.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        //Adding Models as DB sets to create table
+        public DbSet<MainCategory> MainCategories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Colour> Colours { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<ProductColour> ProductColours { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Address> Addresses { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
     }
 }
---
+++ ECommerceProject/Data/SubCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceProject.Data
+{
+    public class SubCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BannerImage { get; set; }
+
+        //Establishing one-many relationship between
+        //MainCategory and SubCategory
+        //or navigation properties
+        public int MainCategoryId { get; set; }
+        public MainCategory MainCategory { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
